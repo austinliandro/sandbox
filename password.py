fdef5de8ce7d24c3ccf7cf26690aab2aa9002f2b
Austin Liandro <austin.liandro@my.jcu.edu.au>
2023-07-17 14:32:26 +0800

    Print testing

--- password.py
+++ password.py
@@ -6,4 +6,4 @@ while len(password) <= MINIMUM_CHARACTER:
     print("The password doesn't meet a minimum length set by a variable. Please try again.")
     password = input("Enter your password: ")
 
-print("Your new password is:","*" * len(password))
+print("Your new password is:","*" *  len(password))
